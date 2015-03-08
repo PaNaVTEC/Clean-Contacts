97140636710faa231e5124e1441282e2cd2468c0
Christian Panadero Martinez <panavtec@gmail.com>
2015-03-08 18:02:36 +0100

    Solved using "assisted injection" checkout: https://groups.google.com/forum/#!topic/dagger-discuss/QgnvmZ-dH9c/discussion

--- app/src/main/java/me/panavtec/cleancontacts/di/AppModule.java
+++ app/src/main/java/me/panavtec/cleancontacts/di/AppModule.java
@@ -1,10 +1,12 @@
 package me.panavtec.cleancontacts.di;
 
 import android.app.Application;
+import android.os.Build;
 import dagger.Module;
 import dagger.Provides;
 import javax.inject.Singleton;
 import me.panavtec.cleancontacts.CleanContactsApp;
+import me.panavtec.cleancontacts.di.qualifiers.ApiLevel;
 
 @Module(
     includes = {
@@ -25,4 +27,8 @@ public class AppModule {
     return app;
   }
 
+  @Provides @Singleton @ApiLevel int provideApiLevel() {
+    return Build.VERSION.SDK_INT;
+  }
+  
 }
