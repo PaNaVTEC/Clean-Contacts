65c1f8a060f51861a4af1e825e70a90e74f4e246
Christian Panadero Martinez <panavtec@gmail.com>
2015-03-09 23:01:07 +0100

    Purgue cache if it needs

--- app/src/main/java/me/panavtec/cleancontacts/di/RepositoryModule.java
+++ app/src/main/java/me/panavtec/cleancontacts/di/RepositoryModule.java
@@ -2,7 +2,6 @@ package me.panavtec.cleancontacts.di;
 
 import dagger.Module;
 import dagger.Provides;
-import java.util.List;
 import java.util.concurrent.TimeUnit;
 import javax.inject.Singleton;
 import me.panavtec.cleancontacts.domain.entities.Contact;
@@ -10,7 +9,6 @@ import me.panavtec.cleancontacts.domain.repository.ContactsRepository;
 import me.panavtec.cleancontacts.repository.caching.strategy.CachingStrategy;
 import me.panavtec.cleancontacts.repository.caching.strategy.ListCachingStrategy;
 import me.panavtec.cleancontacts.repository.contacts.ContactsRepositoryImp;
-import me.panavtec.cleancontacts.repository.contacts.caching.ContactsNotNullCachingStrategy;
 import me.panavtec.cleancontacts.repository.contacts.caching.ContactsTtlCachingStrategy;
 import me.panavtec.cleancontacts.repository.contacts.datasources.ContactsBddDataSource;
 import me.panavtec.cleancontacts.repository.contacts.datasources.ContactsNetworkDataSource;
@@ -26,7 +24,7 @@ public class RepositoryModule {
   @Provides @Singleton ContactsRepository provideContactsRepository(
       ContactsNetworkDataSource networkDataSource, ContactsBddDataSource bddDataSource,
       CachingStrategy<Contact> cachingStrategy,
-      CachingStrategy<List<Contact>> contactListStrategy) {
+      ListCachingStrategy<Contact> contactListStrategy) {
     return new ContactsRepositoryImp(networkDataSource, bddDataSource, cachingStrategy,
         contactListStrategy);
   }
@@ -35,7 +33,8 @@ public class RepositoryModule {
     return new ContactsTtlCachingStrategy(1, TimeUnit.MINUTES);
   }
 
-  @Provides @Singleton CachingStrategy<List<Contact>> provideListContactCachingStrategy() {
-    return new ListCachingStrategy<>(new ContactsNotNullCachingStrategy());
+  @Provides @Singleton ListCachingStrategy<Contact> provideListContactCachingStrategy(
+      CachingStrategy<Contact> strategy) {
+    return new ListCachingStrategy<>(strategy);
   }
 }
