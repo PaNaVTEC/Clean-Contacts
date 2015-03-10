d1e7157fbc300f409e2642feb064f60919fb0e45
Carlos Morera de la Chica <carlos@MacBook-Pro-de-Carlos.local>
2015-03-10 19:51:54 +0100

    Modify test to adapt to new repository imp

--- repository/src/test/java/me/panavtec/cleancontacts/repository/ContactRepositoryTest.java
+++ repository/src/test/java/me/panavtec/cleancontacts/repository/ContactRepositoryTest.java
@@ -1,22 +1,13 @@
 package me.panavtec.cleancontacts.repository;
 
-import junit.framework.Assert;
-
-import org.junit.Before;
-import org.junit.Test;
-import org.mockito.Mock;
-import org.mockito.MockitoAnnotations;
-
 import java.util.Arrays;
 import java.util.List;
-
+import junit.framework.Assert;
 import me.panavtec.cleancontacts.domain.entities.Contact;
 import me.panavtec.cleancontacts.domain.interactors.contacts.exceptions.CannotObtainContactException;
 import me.panavtec.cleancontacts.domain.interactors.contacts.exceptions.RetrieveContactsException;
 import me.panavtec.cleancontacts.domain.repository.ContactsRepository;
 import me.panavtec.cleancontacts.repository.caching.exception.InvalidCacheException;
-import me.panavtec.cleancontacts.repository.caching.strategy.CachingStrategy;
-import me.panavtec.cleancontacts.repository.caching.strategy.list.ListCachingStrategy;
 import me.panavtec.cleancontacts.repository.contacts.ContactsRepositoryImp;
 import me.panavtec.cleancontacts.repository.contacts.datasources.ContactsBddDataSource;
 import me.panavtec.cleancontacts.repository.contacts.datasources.ContactsNetworkDataSource;
@@ -24,51 +15,57 @@ import me.panavtec.cleancontacts.repository.contacts.datasources.exceptions.Cont
 import me.panavtec.cleancontacts.repository.contacts.datasources.exceptions.ObtainBddContactException;
 import me.panavtec.cleancontacts.repository.contacts.datasources.exceptions.ObtainContactsBddException;
 import me.panavtec.cleancontacts.repository.contacts.datasources.exceptions.UnknownObtainContactsException;
+import org.junit.Before;
+import org.junit.Test;
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
 
 import static org.mockito.Mockito.when;
 
 public class ContactRepositoryTest {
 
-    private static final String FAKE_CONTACT_MD5 = "abcdefghijkmnopqrstuvwxyz";
+  private static final String FAKE_CONTACT_MD5 = "abcdefghijkmnopqrstuvwxyz";
 
-    @Mock private ContactsBddDataSource mockContactsBddDataSource;
-    @Mock private ContactsNetworkDataSource mockContactsNetworkDataSource;
-    private CachingStrategy<Contact> contactCachingStrategy = new NotValidCachingStrategyStub();
-    private ListCachingStrategy<Contact> listCachingStrategy = new ListCachingStrategy<>(contactCachingStrategy);
+  @Mock private ContactsBddDataSource mockContactsBddDataSource;
+  @Mock private ContactsNetworkDataSource mockContactsNetworkDataSource;
 
-    private ContactsRepository contactsRepository;
+  private ContactsRepository contactsRepository;
 
-    @Before
-    public void setUp() {
-        MockitoAnnotations.initMocks(this);
-        try {
-            when(mockContactsBddDataSource.obtain(FAKE_CONTACT_MD5)).thenReturn(new Contact());
-            when(mockContactsBddDataSource.obtainContacts()).thenReturn(Arrays.asList(new Contact(), new Contact(), new Contact(), new Contact(), new Contact()));
-            when(mockContactsNetworkDataSource.obtainContacts()).thenReturn(Arrays.asList(new Contact(), new Contact(), new Contact()));
-        } catch (InvalidCacheException | ObtainBddContactException | ContactsNetworkException | UnknownObtainContactsException | ObtainContactsBddException ignored) {
-        }
+  @Before public void setUp() {
+    MockitoAnnotations.initMocks(this);
+    contactsRepository =
+        new ContactsRepositoryImp(mockContactsNetworkDataSource, mockContactsBddDataSource);
+  }
 
-        contactsRepository = new ContactsRepositoryImp(mockContactsNetworkDataSource, mockContactsBddDataSource, contactCachingStrategy, listCachingStrategy);
-
+  @Test public void testObtainContactsWithInvalidCache() {
+    try {
+      when(mockContactsBddDataSource.obtainContacts()).thenThrow(new InvalidCacheException());
+      when(mockContactsNetworkDataSource.obtainContacts()).thenReturn(
+          Arrays.asList(new Contact(), new Contact(), new Contact()));
+      List<Contact> contacts = contactsRepository.obtainContacts();
+      Assert.assertTrue(contacts != null && contacts.size() == 3);
+    } catch (InvalidCacheException | UnknownObtainContactsException | ContactsNetworkException | ObtainContactsBddException | RetrieveContactsException ignored) {
     }
+  }
 
-    @Test
-    public void testObtainContacts() {
-        List<Contact> contacts = null;
-        try {
-            contacts = contactsRepository.obtainContacts();
-            Assert.assertTrue(contacts != null && contacts.size() == 3);
-        } catch (RetrieveContactsException ignored) {
-        }
+  @Test public void testObtainContactsWithValidCache() {
+    try {
+      when(mockContactsBddDataSource.obtainContacts()).thenReturn(
+          Arrays.asList(new Contact(), new Contact(), new Contact(), new Contact(), new Contact()));
+      when(mockContactsNetworkDataSource.obtainContacts()).thenReturn(
+          Arrays.asList(new Contact(), new Contact(), new Contact()));
+      List<Contact> contacts = contactsRepository.obtainContacts();
+      Assert.assertTrue(contacts != null && contacts.size() == 5);
+    } catch (InvalidCacheException | UnknownObtainContactsException | ContactsNetworkException | ObtainContactsBddException | RetrieveContactsException ignored) {
     }
+  }
 
-    @Test
-    public void testObtainContact() {
-        try {
-            Contact contact = contactsRepository.obtain(FAKE_CONTACT_MD5);
-            Assert.assertTrue(contact != null);
-        } catch (CannotObtainContactException ignored) {
-        }
+  @Test public void testObtainContact() {
+    try {
+      when(mockContactsBddDataSource.obtain(FAKE_CONTACT_MD5)).thenReturn(new Contact());
+      Contact contact = contactsRepository.obtain(FAKE_CONTACT_MD5);
+      Assert.assertTrue(contact != null);
+    } catch (InvalidCacheException | ObtainBddContactException | CannotObtainContactException ignored) {
     }
-
+  }
 }
