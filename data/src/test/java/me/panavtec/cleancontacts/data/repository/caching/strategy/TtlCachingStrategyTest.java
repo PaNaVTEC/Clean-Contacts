e9d2bd12296ed18f1a796ab83801b5b7fd989035
Carlos Morera de la Chica <carlosdelachica@gmail.com>
2015-03-11 20:42:53 +0100

    Adds test for Ttl caching strategy

--- data/src/test/java/me/panavtec/cleancontacts/data/repository/caching/strategy/TtlCachingStrategyTest.java
+++ data/src/test/java/me/panavtec/cleancontacts/data/repository/caching/strategy/TtlCachingStrategyTest.java
@@ -1,17 +1,32 @@
 package me.panavtec.cleancontacts.data.repository.caching.strategy;
 
 import java.util.concurrent.TimeUnit;
+import junit.framework.Assert;
 import me.panavtec.cleancontacts.data.repository.caching.strategy.ttl.TtlCachingStrategy;
 import me.panavtec.cleancontacts.data.repository.contacts.datasources.bdd.entities.BddContact;
 import org.junit.Before;
+import org.junit.Test;
 import org.mockito.MockitoAnnotations;
 
 public class TtlCachingStrategyTest {
+
+  private BddContact bddContact;
 
   private TtlCachingStrategy<BddContact> ttlCachingStrategy;
 
   @Before public void setUp() {
     MockitoAnnotations.initMocks(this);
+    bddContact = new BddContact();
     ttlCachingStrategy = new TtlCachingStrategy<>(1, TimeUnit.MINUTES);
+  }
+
+  @Test public void testValidCache() {
+    bddContact.setPersistedTime(System.currentTimeMillis() - TimeUnit.SECONDS.toMillis(59));
+    Assert.assertTrue(ttlCachingStrategy.isValid(bddContact));
+  }
+
+  @Test public void testInvalidCache() {
+    bddContact.setPersistedTime(System.currentTimeMillis() - TimeUnit.SECONDS.toMillis(61));
+    Assert.assertFalse(ttlCachingStrategy.isValid(bddContact));
   }
 }
