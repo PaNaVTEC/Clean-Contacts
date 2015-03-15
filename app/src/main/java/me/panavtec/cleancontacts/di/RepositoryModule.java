8f44260140667c0b9472565432e5d64c28777442
Christian Panadero Martinez <panavtec@gmail.com>
2015-03-15 05:37:31 +0100

    Fixed merge conflicts when moving bitbucket > github

--- app/src/main/java/me/panavtec/cleancontacts/di/RepositoryModule.java
+++ app/src/main/java/me/panavtec/cleancontacts/di/RepositoryModule.java
@@ -2,37 +2,13 @@ package me.panavtec.cleancontacts.di;
 
 import dagger.Module;
 import dagger.Provides;
-<<<<<<< HEAD
-import me.panavtec.cleancontacts.data.repository.contacts.datasources.api.ContactsApiService;
-import me.panavtec.cleancontacts.data.repository.contacts.datasources.api.ContactsNetworkDataSourceImp;
-=======
 import javax.inject.Singleton;
->>>>>>> release/0.8
 import me.panavtec.cleancontacts.domain.repository.ContactsRepository;
 import me.panavtec.cleancontacts.repository.contacts.ContactsRepositoryImp;
 import me.panavtec.cleancontacts.repository.contacts.datasources.ContactsBddDataSource;
 import me.panavtec.cleancontacts.repository.contacts.datasources.ContactsNetworkDataSource;
 
 @Module(
-<<<<<<< HEAD
-        includes = {
-                ApiModule.class,
-                BddModule.class
-        },
-        complete = false,
-        library = true
-)
-public class RepositoryModule {
-
-    @Provides @Singleton ContactsRepository provideGameRepository(ContactsNetworkDataSource networkDataSource,
-                                                                  ContactsBddDataSource bddDataSource) {
-        return new ContactsRepositoryImp(networkDataSource, bddDataSource);
-    }
-
-    @Provides @Singleton ContactsNetworkDataSource provideContactsNetworkDataSource(ContactsApiService apiService) {
-        return new ContactsNetworkDataSourceImp(apiService);
-    }
-=======
     includes = {
         ApiModule.class, BddModule.class
     },
@@ -44,6 +20,4 @@ public class RepositoryModule {
       ContactsNetworkDataSource networkDataSource, ContactsBddDataSource bddDataSource) {
     return new ContactsRepositoryImp(networkDataSource, bddDataSource);
   }
->>>>>>> release/0.8
-
 }
