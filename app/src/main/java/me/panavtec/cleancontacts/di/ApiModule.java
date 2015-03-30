1e1e601537c8c6d814a63ec20230ce084a7c83b0
Christian Panadero Martinez <panavtec@gmail.com>
2015-03-30 15:41:20 +0200

    Fixes dependencies of ApiModule

--- app/src/main/java/me/panavtec/cleancontacts/di/ApiModule.java
+++ app/src/main/java/me/panavtec/cleancontacts/di/ApiModule.java
@@ -22,8 +22,7 @@ import retrofit.converter.GsonConverter;
 
 @Module(
     complete = false,
-    library = true
-)
+    library = true)
 public class ApiModule {
 
   @Provides @Singleton ContactsNetworkDataSource provideContactsNetworkDataSource(
@@ -31,30 +30,52 @@ public class ApiModule {
     return new ContactsNetworkDataSourceImp(apiService);
   }
 
-  @Provides @Singleton ContactsApiService provideApiService(@UserAgent final String userAgent,
-      Endpoint endPoint, @RetrofitLog boolean logTraces) {
+  @Provides @Singleton ContactsApiService provideApiService(GsonConverter gsonConverter,
+      ErrorHandler errorHandler, RequestInterceptor requestInterceptor, Endpoint endPoint,
+      RestAdapter.LogLevel logLevel, OkClient okClient) {
     return new RestAdapter.Builder().setEndpoint(endPoint)
-        .setLogLevel(logTraces ? RestAdapter.LogLevel.FULL : RestAdapter.LogLevel.NONE)
-        .setConverter(new GsonConverter(newGson()))
-        .setClient(new OkClient(new OkHttpClient()))
-        .setRequestInterceptor(new RequestInterceptor() {
-          @Override public void intercept(RequestFacade requestFacade) {
-            requestFacade.addQueryParam("seed", "panavtec");
-            requestFacade.addHeader("User-Agent", userAgent);
-          }
-        })
-        .setErrorHandler(new ErrorHandler() {
-          @Override public Throwable handleError(RetrofitError cause) {
-            ApiNetworkError networkError = new ApiNetworkError();
-            networkError.setStackTrace(cause.getStackTrace());
-            return networkError;
-          }
-        })
+        .setLogLevel(logLevel)
+        .setConverter(gsonConverter)
+        .setClient(okClient)
+        .setRequestInterceptor(requestInterceptor)
+        .setErrorHandler(errorHandler)
         .build()
         .create(ContactsApiService.class);
   }
 
-  public static Gson newGson() {
+  @Provides @Singleton RequestInterceptor provideRequestInterceptor(
+      @UserAgent final String userAgent) {
+    return new RequestInterceptor() {
+      @Override public void intercept(RequestFacade requestFacade) {
+        requestFacade.addQueryParam("seed", "panavtec");
+        requestFacade.addHeader("User-Agent", userAgent);
+      }
+    };
+  }
+
+  @Provides @Singleton ErrorHandler provideContactsApiErrorHandler() {
+    return new ErrorHandler() {
+      @Override public Throwable handleError(RetrofitError cause) {
+        ApiNetworkError networkError = new ApiNetworkError();
+        networkError.setStackTrace(cause.getStackTrace());
+        return networkError;
+      }
+    };
+  }
+
+  @Provides @Singleton RestAdapter.LogLevel provideLogLevel(@RetrofitLog boolean logTraces) {
+    return logTraces ? RestAdapter.LogLevel.FULL : RestAdapter.LogLevel.NONE;
+  }
+
+  @Provides @Singleton OkClient provideOkClient() {
+    return new OkClient(new OkHttpClient());
+  }
+
+  @Provides @Singleton GsonConverter provideGsonConverter(Gson gson) {
+    return new GsonConverter(gson);
+  }
+
+  @Provides @Singleton Gson provideGsonParser() {
     return new GsonBuilder().excludeFieldsWithoutExposeAnnotation().create();
   }
 }
