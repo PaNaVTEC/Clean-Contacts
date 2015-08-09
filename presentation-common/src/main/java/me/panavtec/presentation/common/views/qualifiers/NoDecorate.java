d88ddd317c9fee101c9bed75db1aea8eb20af7a8
Christian Panadero <panavtec@gmail.com>
2015-08-09 11:33:04 +0100

    Added small doc about @NoDecorate annotation

--- presentation-common/src/main/java/me/panavtec/presentation/common/views/qualifiers/NoDecorate.java
+++ presentation-common/src/main/java/me/panavtec/presentation/common/views/qualifiers/NoDecorate.java
@@ -5,6 +5,10 @@ import java.lang.annotation.Retention;
 import java.lang.annotation.RetentionPolicy;
 import java.lang.annotation.Target;
 
+/**
+ * Use this class in case you want to no decorate any of the methods of your View Interfaces.
+ * For example the init Ui methods that are you sure are in the same thread.
+ */
 @Target(ElementType.METHOD) @Retention(RetentionPolicy.SOURCE)
 public @interface NoDecorate {
 }
