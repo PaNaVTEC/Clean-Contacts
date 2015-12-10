a39ae7202f1a4b4440012e1a43b39d3750c7f1dc
Carlos Morera de la Chica <carlosdelachica@gmail.com>
2015-12-10 19:27:03 +0000

    Removed unused exceptions

--- domain/src/main/java/me/panavtec/cleancontacts/domain/interactors/contacts/exceptions/ObtainContactException.java
+++
@@ -1,7 +0,0 @@
-package me.panavtec.cleancontacts.domain.interactors.contacts.exceptions;
-
-import java.util.concurrent.ExecutionException;
-
-public class ObtainContactException extends ExecutionException {
-
-}
