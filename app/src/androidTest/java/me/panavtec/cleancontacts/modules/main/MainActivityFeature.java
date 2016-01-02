6744c272b3e3d85095df303a3d9d4f8b5a3a7cc5
Christian Panadero <panavtec@gmail.com>
2016-01-02 22:51:50 +0100

    Try to use the ThreadPoolExecutor for the InteractorInvoker because Espresso uses that to see if there are any idling resources

--- app/src/androidTest/java/me/panavtec/cleancontacts/modules/main/MainActivityFeature.java
+++ app/src/androidTest/java/me/panavtec/cleancontacts/modules/main/MainActivityFeature.java
@@ -1,9 +1,12 @@
 package me.panavtec.cleancontacts.modules.main;
 
+import android.app.Application;
+import android.support.test.InstrumentationRegistry;
 import android.support.test.espresso.intent.Intents;
 import android.support.test.runner.AndroidJUnit4;
 import android.support.v7.widget.RecyclerView;
 import android.test.suitebuilder.annotation.LargeTest;
+import me.panavtec.cleancontacts.ActivityResumedIdlingResource;
 import me.panavtec.cleancontacts.DaggerActivityTestRule;
 import me.panavtec.cleancontacts.R;
 import me.panavtec.cleancontacts.modules.detail.DetailActivity;
@@ -12,6 +15,8 @@ import org.junit.Test;
 import org.junit.runner.RunWith;
 
 import static android.support.test.espresso.Espresso.onView;
+import static android.support.test.espresso.Espresso.registerIdlingResources;
+import static android.support.test.espresso.Espresso.unregisterIdlingResources;
 import static android.support.test.espresso.action.ViewActions.click;
 import static android.support.test.espresso.contrib.RecyclerViewActions.actionOnItemAtPosition;
 import static android.support.test.espresso.intent.Intents.intended;
@@ -25,6 +30,8 @@ import static org.junit.Assert.assertThat;
   @Rule public DaggerActivityTestRule<MainActivity> activityTestRule =
       new DaggerActivityTestRule<>(MainActivity.class);
 
+  private ActivityResumedIdlingResource idleUntilResume;
+
   @Test public void load_a_contact_list_when_screen_is_resumed() {
     RecyclerView recyclerView = activityTestRule.getActivity().recyclerView;
     assertThat(recyclerView.getChildCount(), is(3));
@@ -37,4 +44,16 @@ import static org.junit.Assert.assertThat;
     Intents.release();
   }
 
+  private void waitUntilActivityIsResumed() {
+    Application app = (Application) InstrumentationRegistry.getInstrumentation().getTargetContext().getApplicationContext();
+    idleUntilResume = new ActivityResumedIdlingResource();
+    app.registerActivityLifecycleCallbacks(idleUntilResume);
+    registerIdlingResources(idleUntilResume);
+  }
+
+  private void removeIdlingResources() {
+    Application app = (Application) InstrumentationRegistry.getInstrumentation().getTargetContext().getApplicationContext();
+    unregisterIdlingResources(idleUntilResume);
+    app.unregisterActivityLifecycleCallbacks(idleUntilResume);
+  }
 }
